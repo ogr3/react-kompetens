6a23873a4f13b559b8eab9e02daa2f600ecc5317
Gabriel Özbek <gabrielozbek@Gabriels-MacBook-Pro.local>
2018-11-28 00:30:00 +0100

    Removed redux from index.js in live2

--- live2/src/index.js
+++ live2/src/index.js
@@ -2,17 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
 
-const store = createStore(
-  () => {},
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
